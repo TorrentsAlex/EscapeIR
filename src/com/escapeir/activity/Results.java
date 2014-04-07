b758472e61743d5bc712414d9ae9ebcbdfdc63a0
alexrufus <torrents.alex1@gmail.com>
2014-04-07 18:18:33 +0200

    Create Results Activity (does nothing)

---
+++ src/com/escapeir/activity/Results.java
@@ -0,0 +1,17 @@
+package com.escapeir.activity;
+
+import com.example.escapeir.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Results extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result_layout);
+	}
+	
+}
