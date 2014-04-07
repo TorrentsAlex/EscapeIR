d9c2c8f0f011d803ba0f4efb424d77ae2848ab53
alexrufus <torrents.alex1@gmail.com>
2014-04-07 18:16:05 +0200

    Change searchCompleted, and Intent when the game finish

--- src/com/escapeir/activity/Camera.java
+++ src/com/escapeir/activity/Camera.java
@@ -2,6 +2,7 @@ package com.escapeir.activity;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,109 +24,122 @@ import com.catchoom.CatchoomTracking;
 import com.escapeir.application.EscapeIRApplication;
 import com.example.escapeir.R;
 
-public class Camera extends CatchoomActivity implements OnClickListener, CatchoomResponseHandler, CatchoomImageHandler {
+public class Camera extends CatchoomActivity implements OnClickListener,
+		CatchoomResponseHandler, CatchoomImageHandler {
 
 	private CatchoomCloudRecognition cloudRecognition;
 	private CatchoomTracking catchoomTracking;
 	private CatchoomCamera catchoomCamera;
-	
+
 	private Button btnHelp;
 	private Button btnPhoto;
-	
+
 	@Override
 	public void onPostCreate() {
-        View mainLayout= (View) getLayoutInflater().inflate(R.layout.photo_layout, null);
-        CatchoomCameraView cameraView = (CatchoomCameraView) mainLayout.findViewById(R.id.camera_preview);
-        super.setCameraView(cameraView);
-        setContentView(mainLayout);
+		View mainLayout = (View) getLayoutInflater().inflate(
+				R.layout.photo_layout, null);
+		CatchoomCameraView cameraView = (CatchoomCameraView) mainLayout
+				.findViewById(R.id.camera_preview);
+		super.setCameraView(cameraView);
+		setContentView(mainLayout);
 
-        //Initialize the SDK.
-        CatchoomSDK.init(getApplicationContext(),this);
-       
-        cloudRecognition= CatchoomSDK.getCloudRecognition();
-        cloudRecognition.setResponseHandler(this);
+		// Initialize the SDK.
+		CatchoomSDK.init(getApplicationContext(), this);
 
-        cloudRecognition.connect(EscapeIRApplication.escapeToken);
+		cloudRecognition = CatchoomSDK.getCloudRecognition();
+		cloudRecognition.setResponseHandler(this);
 
-        catchoomTracking = CatchoomSDK.getTracking();
-        
-        catchoomCamera = CatchoomSDK.getCamera();
-        catchoomCamera.setImageHandler(this);
-        
-        btnHelp =(Button) findViewById(R.id.btn_help);
-        btnPhoto=(Button) findViewById(R.id.btn_photo);
-        btnHelp.setOnClickListener(this);
-        btnPhoto.setOnClickListener(this);
-        
-        //cargar los arrays
+		cloudRecognition.connect(EscapeIRApplication.escapeToken);
+
+		catchoomTracking = CatchoomSDK.getTracking();
+
+		catchoomCamera = CatchoomSDK.getCamera();
+		catchoomCamera.setImageHandler(this);
+
+		btnHelp = (Button) findViewById(R.id.btn_help);
+		btnPhoto = (Button) findViewById(R.id.btn_photo);
+		btnHelp.setOnClickListener(this);
+		btnPhoto.setOnClickListener(this);
+
+		// cargar los arrays
 	}
 
 	@Override
 	public void connectCompleted() {
 		Log.d(EscapeIRApplication.TAG, "connect Completed");
-		
+
 	}
 
 	@Override
 	public void requestFailedResponse(int arg0,
 			CatchoomCloudRecognitionError arg1) {
 		Log.i(EscapeIRApplication.TAG, arg1.getErrorMessage());
-		
+
 	}
 
 	@Override
 	public void searchCompleted(ArrayList<CatchoomCloudRecognitionItem> results) {
-        boolean haveContent = false;
-    	Log.i(EscapeIRApplication.TAG, "searchCompleted");
-    	
-        // Look for trackable results
-        for (CatchoomCloudRecognitionItem item : results) {
-        	Log.i(EscapeIRApplication.TAG, item.getItemName());
+		boolean haveContent = false;
+		Log.i(EscapeIRApplication.TAG, "searchCompleted");
 
-            // Setup the AR experience with content provided in the response
-            if (item.isAR()) {
-                CatchoomARItem itemAR = (CatchoomARItem) item;
-                if (itemAR.getContents().size() > 0) {
+		// Look for trackable results
+		for (CatchoomCloudRecognitionItem item : results) {
+			Log.i(EscapeIRApplication.TAG, item.getItemName());
 
-                    catchoomTracking.addItem(itemAR);
-                    catchoomTracking.startTracking();
-                }
-            }else {
-            	if(item.getItemName().equals(EscapeIRApplication.REFERENCE_ROOM[EscapeIRApplication.COUNT_REFERENCE])){
-            		EscapeIRApplication.COUNT_REFERENCE++;
-            		Toast.makeText(this,  item.getItemName() , Toast.LENGTH_SHORT).show();
-            	}else {
-            		Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
-            	}
-            }
-        }
-        catchoomCamera.restartCameraPreview();
-    	Log.i(EscapeIRApplication.TAG, "restart Camera");
+			if (item.getItemName()
+					.equals(EscapeIRApplication.REFERENCE_ROOM[EscapeIRApplication.COUNT_REFERENCE])) {
+				EscapeIRApplication.COUNT_REFERENCE++;
+				// Item Augmented Reality
+				if (item.isAR()) {
+					CatchoomARItem itemAR = (CatchoomARItem) item;
+					if (itemAR.getContents().size() > 0) {
+						catchoomTracking.addItem(itemAR);
+						catchoomTracking.startTracking();
+					}
+				} else { // Item Image Recognition
+					Toast.makeText(this, item.getItemName(), Toast.LENGTH_SHORT)
+					.show();
+				}
+				// finish the game, go to results
+				if(EscapeIRApplication.COUNT_REFERENCE == EscapeIRApplication.REFERENCE_ROOM.length) {
+					Intent intent = new Intent(this, Results.class);
+					startActivity(intent);
+					finish();
+				}
+
+			} else {
+				Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+			}
+		}
+		catchoomCamera.restartCameraPreview();
+		Log.i(EscapeIRApplication.TAG, "restart Camera");
 
 	}
 
 	@Override
 	public void requestImageError(String error) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void requestImageReceived(CatchoomImage image) {
-    	Log.i(EscapeIRApplication.TAG, "searchImage");
+		Log.i(EscapeIRApplication.TAG, "searchImage");
 
-        cloudRecognition.searchWithImage(EscapeIRApplication.escapeToken, image);
+		cloudRecognition
+				.searchWithImage(EscapeIRApplication.escapeToken, image);
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()) {
-		case R.id.btn_help: break;
+		switch (v.getId()) {
+		case R.id.btn_help:
+			break;
 		case R.id.btn_photo:
 			catchoomCamera.takePicture();
 			break;
 		}
-		
+
 	}
 
 }
