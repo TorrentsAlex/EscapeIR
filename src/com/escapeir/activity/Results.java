7abbb21625c92468e615a783e8b36b6d00c4d84f
alexrufus <torrents.alex1@gmail.com>
2014-04-16 22:51:08 +0200

    add adapter for listview

--- src/com/escapeir/activity/Results.java
+++ src/com/escapeir/activity/Results.java
@@ -1,23 +1,41 @@
 package com.escapeir.activity;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.catchoom.CatchoomCloudRecognitionItem;
+import com.escapeir.application.EscapeIRApplication;
+import com.escapeir.server.Connect;
+import com.escapeir.userclass.User;
+import com.escapeir.util.ItemAdapter;
 import com.example.escapeir.R;
 
 public class Results extends Activity {
 	private TextView userTime;
-
+	private Connect connect = new Connect();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.result_layout);
 		
 		userTime = (TextView) findViewById(R.id.timeUser);
+		ListView viewList = (ListView) findViewById(R.id.listView1);
 		
 		userTime.setText(getIntent().getExtras().getString("time"));
+		viewList.setAdapter(new ItemAdapter(this,
+				R.layout.result_list, EscapeIRApplication.usersServer));
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		EscapeIRApplication.usersServer.clear();
+		finish();
 	}
 	
 }
