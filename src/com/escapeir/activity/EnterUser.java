6c925c63307e3463e5fd88fbcf8be99517d9bafc
alexrufus <torrents.alex1@gmail.com>
2014-04-17 16:59:17 +0200

    create check user and textview if user is invalid

--- src/com/escapeir/activity/EnterUser.java
+++ src/com/escapeir/activity/EnterUser.java
@@ -1,7 +1,6 @@
 package com.escapeir.activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.escapeir.application.EscapeIRApplication;
 import com.escapeir.util.Preferences;
@@ -18,11 +18,12 @@ public class EnterUser extends Activity implements OnClickListener {
 	
 	private EditText editUser;
 	private Button buttonUser;
+	private TextView textInvalidUser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_user);
+		setContentView(R.layout.layout_user);
 		
 		if(!Preferences.setUsername().equals(EscapeIRApplication.preferencesUserDefault)) {
 			Intent mainIntent = new Intent(this, Main.class);
@@ -30,7 +31,7 @@ public class EnterUser extends Activity implements OnClickListener {
 			finish();
 		}
 
-		//TODO: introducir el nickname y guardarlo en las preferences
+		textInvalidUser = (TextView) findViewById(R.id.txt_invalid_user);
 		editUser = (EditText) findViewById(R.id.edit_user);
 		buttonUser = (Button) findViewById(R.id.btn_name);
 		buttonUser.setOnClickListener(this);
@@ -40,17 +41,26 @@ public class EnterUser extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		if(v.getId() == R.id.btn_name) {
+			String user = editUser.getText().toString(); 
 			
-			if(null != EscapeIRApplication.preferencesUser) {
-				Log.i(EscapeIRApplication.TAG,"save user name");
-				Preferences.saveUsername(editUser.getText().toString());
+			if (!user.isEmpty()) {
+				
+				textInvalidUser.setVisibility(View.GONE);
+				
+				if(null != EscapeIRApplication.preferencesUser) {
+					Log.i(EscapeIRApplication.TAG,"save user name");
+					Preferences.saveUsername(editUser.getText().toString());
+				}
+				
+				editUser.setText("");
+				Log.i(EscapeIRApplication.TAG,"go Main class");
+				Intent mainIntent = new Intent(this, Main.class);
+				startActivity(mainIntent);
+				finish();
+			// User Not valid
+			} else {
+				textInvalidUser.setVisibility(View.VISIBLE);
 			}
-			
-			editUser.setText("");
-			Log.i(EscapeIRApplication.TAG,"go Main.class");
-			Intent mainIntent = new Intent(this, Main.class);
-			startActivity(mainIntent);
-			finish();
 		}
 	}
 
