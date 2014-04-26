6debd15e6507bf7e8d834a9e7b551cb722d53040
alexrufus <torrents.alex1@gmail.com>
2014-04-26 17:14:19 +0200

    modified

--- src/com/escapeir/activity/Results.java
+++ src/com/escapeir/activity/Results.java
@@ -1,44 +1,73 @@
 package com.escapeir.activity;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.catchoom.CatchoomCloudRecognitionItem;
 import com.escapeir.application.EscapeIRApplication;
-import com.escapeir.server.Connect;
-import com.escapeir.userclass.User;
 import com.escapeir.util.ItemAdapter;
 import com.example.escapeir.R;
 
-public class Results extends Activity {
+public class Results extends Activity implements OnClickListener {
 	private TextView userTime;
+	private TextView txtYourTime;
+	private Button btnReturn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.result_layout);
-		
-		
+
+		txtYourTime = (TextView) findViewById(R.id.txt_your_time);
 		userTime = (TextView) findViewById(R.id.time_user);
-		
+		btnReturn = (Button) findViewById(R.id.btn_return);
 		ListView viewList = (ListView) findViewById(R.id.listView1);
 		
-		userTime.setText(getIntent().getExtras().getString("time"));
+		btnReturn.setOnClickListener(this);
 		
-		viewList.setAdapter(new ItemAdapter(this,
-				R.layout.result_list, EscapeIRApplication.usersServer));
-		
+		if (getIntent().getExtras().getString("time").equals(null)) {
+			txtYourTime.setVisibility(View.GONE);
+			userTime.setVisibility(View.GONE);
+		} else {
+			userTime.setText(getIntent().getExtras().getString("time"));
+		}
+
+		viewList.setAdapter(new ItemAdapter(this, R.layout.result_list,
+				EscapeIRApplication.usersServer));
+
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		EscapeIRApplication.usersServer.clear();
+		txtYourTime.setVisibility(View.VISIBLE);
+		userTime.setVisibility(View.VISIBLE);
 		finish();
 	}
-	
+
+	@Override
+	public void onBackPressed() {
+		txtYourTime.setVisibility(View.VISIBLE);
+		userTime.setVisibility(View.VISIBLE);
+		finish();
+		
+		super.onBackPressed();
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		switch (arg0.getId()) {
+		case R.id.btn_return:
+			finish();
+			break;
+		}
+
+	}
+
 }
