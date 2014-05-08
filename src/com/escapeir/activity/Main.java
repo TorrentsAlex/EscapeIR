9d1c19ce4808fefa4e7b6af215f18a2b2bc1bca7
alexrufus <torrents.alex1@gmail.com>
2014-05-08 22:34:23 +0200

    add method for check if network is available,

--- src/com/escapeir/activity/Main.java
+++ src/com/escapeir/activity/Main.java
@@ -1,14 +1,17 @@
 package com.escapeir.activity;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.escapeir.application.EscapeIRApplication;
 import com.escapeir.logic.Logic;
@@ -50,22 +53,41 @@ public class Main extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		Intent cameraIntent = new Intent(this, Camera.class);
+		
 
 		switch (v.getId()) {
 		case R.id.btn_kitchen:
-			Logic.chargeArray(EscapeIRApplication.MODE_KITCHEN, this);
-
-			startActivity(cameraIntent);
+			//Logic.chargeArray(EscapeIRApplication.MODE_KITCHEN, this);
+			//tartActivity(cameraIntent);
+			Toast.makeText(this, "Estará disponible próximanente", Toast.LENGTH_SHORT).show();
 			break;
-		case R.id.btn_classroom:
+		case R.id.btn_classroom:	
+			if (!isOnline()) { 
+				Toast.makeText(this, "No hay conexión a internet", Toast.LENGTH_SHORT).show();
+				return;
+			}
 			Logic.chargeArray(EscapeIRApplication.MODE_CLASSROOM, this);
-
 			startActivity(cameraIntent);
 			break;
 		case R.id.btn_show_ranking:
+			if (!isOnline()) { 
+				Toast.makeText(this, "No hay conexión a internet", Toast.LENGTH_SHORT).show();
+				return;
+			}
 			new Connect(this,Main.this).execute(false);
-
 		}
 	}
-
+	/**
+	 *  
+	 * */
+	private boolean isOnline() {
+	    // Check if network is available
+		ConnectivityManager cm =
+	        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+	        return true;
+	    }
+	    return false;
+	}
 }
