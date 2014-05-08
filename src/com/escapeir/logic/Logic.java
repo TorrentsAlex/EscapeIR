305a65ddac00d6fd9cb66ad2ff8ad6dd92fa8bd4
alexrufus <torrents.alex1@gmail.com>
2014-05-08 22:30:13 +0200

    add reference, reward and intro classroom

--- src/com/escapeir/logic/Logic.java
+++ src/com/escapeir/logic/Logic.java
@@ -7,11 +7,15 @@ import com.escapeir.application.EscapeIRApplication;
 import com.example.escapeir.R;
 
 public class Logic  {
+	
 	public static void chargeArray(int room, Context context) {
 		switch(room) {
 		case EscapeIRApplication.MODE_CLASSROOM:
-			EscapeIRApplication.CHOOSEN_ROOM = context.getResources().getStringArray(R.array.reference_prueba);
+			EscapeIRApplication.CHOOSEN_ROOM = context.getResources().getStringArray(R.array.reference_classroom);
 			EscapeIRApplication.GUIDE_ROOM = context.getResources().getStringArray(R.array.guide_classroom);
+			EscapeIRApplication.REWARD_ROOM = context.getResources().getStringArray(R.array.reward_classroom);
+			EscapeIRApplication.INTRO_ROOM = context.getResources().getString(R.string.intro_body_classroom);
+			
 			for(byte i=0;i<EscapeIRApplication.CHOOSEN_ROOM.length;i++) {
 			
 				Log.i(EscapeIRApplication.TAG, EscapeIRApplication.CHOOSEN_ROOM[i] + i);
@@ -19,9 +23,6 @@ public class Logic  {
 			break;
 		case EscapeIRApplication.MODE_KITCHEN:
 			EscapeIRApplication.CHOOSEN_ROOM = context.getResources().getStringArray(R.array.reference_prueba);
-			for(byte i=0;i<EscapeIRApplication.CHOOSEN_ROOM.length;i++) {
-				Log.i(EscapeIRApplication.TAG, EscapeIRApplication.CHOOSEN_ROOM[i]+ i);
-			}
 			break;
 		}
 	}
