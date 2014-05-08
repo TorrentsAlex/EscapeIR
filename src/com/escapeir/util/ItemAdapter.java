77b7b642dca4ed7c3d000f1b86e171d81ff7315b
alexrufus <torrents.alex1@gmail.com>
2014-05-08 22:33:26 +0200

    add position for top users

--- src/com/escapeir/util/ItemAdapter.java
+++ src/com/escapeir/util/ItemAdapter.java
@@ -2,16 +2,15 @@ package com.escapeir.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.annotation.SuppressLint;
+
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import com.escapeir.application.EscapeIRApplication;
+
 import com.escapeir.userclass.User;
 import com.example.escapeir.R;
 
@@ -41,12 +40,21 @@ public class ItemAdapter extends ArrayAdapter<User> {
 		
 		// set colors for the best results 
 		LinearLayout listLayout = (LinearLayout) convertView.findViewById(R.id.layout_list);
+		TextView textPosition = (TextView) convertView.findViewById(R.id.text_list_position);
 		if (position == 0){
 			listLayout.setBackgroundResource(R.color.gold);
+			textPosition.setText(R.string.primero);
+			textPosition.setVisibility(View.VISIBLE);
+			
 		}else if (position == 1) {
 			listLayout.setBackgroundResource(R.color.silver);
+			textPosition.setText(R.string.segundo);
+			textPosition.setVisibility(View.VISIBLE);
+		
 		}else if (position == 2) {
 			listLayout.setBackgroundResource(R.color.bronze);
+			textPosition.setText(R.string.tercero);
+			textPosition.setVisibility(View.VISIBLE);
 		}
 
 		return convertView;
